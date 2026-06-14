99f43cb08a64d584393daacd99ea0e2a1cbc6ab1
VladlenAfonin <vladlenafonin.university@yandex.ru>
2026-06-14 15:05:40 +0300

    add stark figures

--- results/build.sh
+++ results/build.sh
@@ -5,18 +5,29 @@ set -eu
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 BUILD_DIR="${SCRIPT_DIR}/build"
 PARTY=""
+PROTOCOL=""
 
-if [[ "$#" -ne 1 ]]; then
-    >&2 echo "error: required 1 argument: PARTY (available options: prover, verifier)"
+if [[ "$#" -ne 2 ]]; then
+    >&2 echo "error: required 2 arguments: PARTY (available options: prover, verifier) and PROTOCOL (available options: fri, stark)"
     exit 1
 fi
 
 case "$1" in
-    "prover" | "verifier")
-        PARTY="$1"
+    "fri" | "stark")
+        PROTOCOL="$1"
         ;;
     *)
-        >&2 echo "error: unknown party: \"$1\" (available options: prover, verifier)"
+        >&2 echo "error: unknown protocol: \"$1\" (available options: fri, stark)"
+        exit 1
+        ;;
+esac
+
+case "$2" in
+    "prover" | "verifier")
+        PARTY="$2"
+        ;;
+    *)
+        >&2 echo "error: unknown party: \"$2\" (available options: prover, verifier)"
         exit 1
         ;;
 esac
@@ -27,14 +38,14 @@ LATEXMK=(
     "--lualatex"
     "--outdir=${BUILD_DIR}"
     "--auxdir=${BUILD_DIR}"
-    "${SCRIPT_DIR}/${PARTY}.tex"
+    "${SCRIPT_DIR}/${PROTOCOL}-${PARTY}.tex"
 )
 "${LATEXMK[@]}"
 
 MAGICK_JPG=(
     "magick"
     "-density" "600"
-    "${BUILD_DIR}/${PARTY}.pdf"
-    "${BUILD_DIR}/${PARTY}.jpg"
+    "${BUILD_DIR}/${PROTOCOL}-${PARTY}.pdf"
+    "${BUILD_DIR}/${PROTOCOL}-${PARTY}.jpg"
 )
 "${MAGICK_JPG[@]}"
